7eeb34f29d8df5015eadc3db7505728da040b9c2
Ilhom <khaitovilhom04@gmail.com>
2024-09-13 17:35:53 +0500

    exem

--- src/redux/api/productsApi.ts
+++ src/redux/api/productsApi.ts
@@ -1,14 +1,15 @@
 import {api} from "./index.ts";
+import {Recipe} from "../../types";
 
 
 const productsApi = api.injectEndpoints?.({
   endpoints: (build) => ({
-    allProducts: build.query({
+    allProducts: build.query<{recipes: Recipe[]}, void>({
       query: () => ({
         url: "/recipes",
       })
     }),
-    getProduct: build.query({
+    getProduct: build.query<Recipe, number>({
       query: (id) => ({
         url: `/recipes/${id}`,
       })
